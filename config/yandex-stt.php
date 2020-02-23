9597ee83c9208125ba5374297d0950fb1e4b8fda
vagrant <vagrant@homestead>
2020-02-23 18:21:19 +0000

    удаление файлов специфичных для laravel(будет реализовано отдельным пакетом)

--- config/yandex-stt.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'key' => env('YANDEX_CLOUD_STT_KEY', ''),
-    'topic'=> env('YANDEX_CLOUD_STT_KEY', 'queries'),
-    'lang' => env('YANDEX_CLOUD_STT_KEY', 'ru-RU'),
-    'tmp-dir' => storage_path('app/tmp')
-];
